02ba7c849e770c13adc98563d9acebf23674fb21
AG <gastandrzej@gmail.com>
2022-12-19 15:07:43 +0100

    Secend

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import pandas as pd
 import pivottablejs as piv
 import webbrowser
 from tkinter import *
+import openpyxl
 
 
 class Pivoncjusz:
     def __init__(self = None):
 
         root = Tk()
-        root.geometry("400x200")
+        root.geometry("500x300")
         root.title("Pivoncjusz Tabeliusz")
 
 
@@ -29,17 +30,37 @@ class Pivoncjusz:
         inputer_separator = Entry(root, width=10, textvariable=inputer_separator_text)
         inputer_separator.pack()
 
+        l3 = Label(root, text="Put in here your xlsx file :)")
+        l3.config(font=("Coutier", 10))
+        l3.pack()
+
+        inputer_file_text_excel = StringVar()
+        inputer_file_xlsx = Entry(root, width=10, textvariable=inputer_file_text_excel)
+        inputer_file_xlsx.pack()
+
         def funcer_creator():
             df = pd.read_csv(f"{inputer_file_text.get()}.csv", sep=f"{inputer_separator_text.get()}")
             piv.pivot_ui(df)
             return webbrowser.open("pivottablejs.html")
 
+        def funcer_xls_creator():
+            df = pd.read_excel(f"excel_files\{inputer_file_text_excel.get()}.xlsx")
+            df.to_csv(r"excelpiv.csv",encoding="utf8", sep=";")
+            dff = pd.read_csv("excelpiv.csv", sep=";")
+            piv.pivot_ui(dff)
+            return webbrowser.open("pivottablejs.html")
+
 
         Button1 = Button(root, height=2,
                          width=20,
-                         text="Show",
+                         text="From CSV",
                          command= funcer_creator)
         Button1.pack()
+        Button2 = Button(root, height=2,
+                         width=20,
+                         text="From XLS",
+                         command= funcer_xls_creator)
+        Button2.pack()
 
 
         mainloop()
